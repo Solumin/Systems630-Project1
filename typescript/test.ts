0b16b108671ae2868432d4999ee3ba3ef7ab481a
Teddy Sudol <magesolumin@gmail.com>
2014-10-08 21:10:48 -0400

    Improved unmarshal support
    - Added Null object.
    -Note that "0" in marshal and "N" refer to 2 different things.
    - This is still under investigation
    - Added Py_CodeObject to represent unmarshalled .pyc files/code objects
    - TODO: Add stack frame class? Also, all the other TODOs in this file
    - Most of the types are implemented now :+1:

--- typescript/test.ts
+++ typescript/test.ts
@@ -2,6 +2,75 @@
 // converts into a Python code object.
 var fs = require('fs');
 
+// Null is an empty value. Mostly used in the interpreter for dictionaries.
+// Python has a single null object called "None".
+class NullSingleton {
+    private static _instance: NullSingleton;
+
+    constructor() {
+        if(NullSingleton._instance) {
+            throw new Error("Null is already instantiated. Use get() instead.");
+        }
+        NullSingleton._instance = this;
+    }
+
+    get(): NullSingleton {
+        if(NullSingleton._instance == null) {
+            NullSingleton._instance = new NullSingleton();
+            return NullSingleton._instance;
+        }
+    }
+}
+var None = NullSingleton.get();
+
+class Py_CodeObject {
+    argcount: number;
+    cellvars: string[];
+    code: string;
+    consts: any[];
+    filename: string;
+    firstlineno: number;
+    flags: number;
+    freevars: string[];
+    lnotab: string;
+    name: string;
+    names: string[];
+    nlocals: number;
+    stacksize: number;
+    varnames: string[];
+
+    // Args are in marshal order
+    constructor(argcount: number,
+                nlocals: number,
+                stacksize: number,
+                flags: number,
+                code: string,
+                consts: any[],
+                names: string[],
+                varnames: string[],
+                freevars: string[],
+                cellvars: string[],
+                filename: string,
+                name: string,
+                firstlineno: number,
+                lnotab: string) {
+        this.argcount = argcount;
+        this.cellvars = cellvars;
+        this.code = code;
+        this.consts = consts;
+        this.filename = filename;
+        this.firstlineno = firstlineno;
+        this.flags = flags;
+        this.freevars = freevars;
+        this.lnotab = lnotab;
+        this.name = name;
+        this.names = names;
+        this.nlocals = nlocals;
+        this.stacksize = stacksize;
+        this.varnames = varnames;
+    }
+}
+
 class Unmarshaller {
     // How far we are into the buffer
     index: number;
@@ -13,6 +82,8 @@ class Unmarshaller {
     magicNumber: number;
     // Date of compilation
     date: Date;
+    // The list of "interalized" strings
+    internedStrs: string[];
     // The output of unmarshalling the .pyc file
     output;
 
@@ -92,12 +163,20 @@ class Unmarshaller {
         var res;
         switch (unit) {
             // Constants
-            case "0": // Null
-            case "F": // False
+            case "0": // Null. TODO: Investigate usage. Dictionary keys?
             case "N": // None
+                res = None;
+                break;
+            case "F": // False
+                res = false;
+                break;
             case "S": // StopIteration Exception (TODO: double check this)
+                throw new Error("StopIteration is pending investigation");
             case "T": // True
+                res = true;
+                break;
             case ".": // Ellipsis object (TODO: double check this)
+                throw new Error("Ellipsis is not yet implemented");
                 break;
             // Numbers
             // case "f": // "old" marshal-format float
@@ -108,19 +187,28 @@ class Unmarshaller {
                 res = this.readInt32();
                 break;
             case "I": // 64-bit integer (signed)
+                throw new Error("We're still working on 64-bit support");
             case "l": // 32-bit long (unsigned integer?)
                 res = this.readInt32();
                 break;
             // case "x": // "old" marshal-format complex
             case "y": // complex number
+                throw new Error("Complex numbers are not supported");
                 break;
             // Strings
             case "R": // Reference to interned string
+                var index = this.readInt32();
+                res = this.internedStrs(index);
+                break;
             case "s": // plain string. length (int 32) + bytes
                 var length = this.readInt32();
                 res = this.readString(length);
                 break;
             case "t": // interned string, stored in an array
+                var length = this.readInt32();
+                res = this.readString(length);
+                this.internedStrs.push(res);
+                break;
             case "u": // utf-8 string
                 var length = this.readInt32();
                 res = this.readUnicodeString(length);
@@ -128,13 +216,37 @@ class Unmarshaller {
             // Collections
             case "(": // tuple
             case "[": // list
+                var length = this.readInt32();
+                res = [];
+                for (x = 0; x < length; x++) {
+                    res.push(this.unmarshal());
+                }
+                break;
                 // Now built-in classes, not types:
-            // case "{": dictionary
-            // case "<": set
-            // case ">": frozenset
-                break;                
+            case "{": dictionary
+            case "<": set
+            case ">": frozenset
+                throw new Error("Dicts, sets and frozensets are pending investigation");
+                break;
             // Code Objects:
             case "c":
+                var argc = this.readInt32();
+                var nlocals = this.readInt32();
+                var stacksize = this.readInt32();
+                var flags = this.readInt32();
+                var codestr: string = this.unmarshal();
+                var consts: string[] = this.unmarshal();
+                var names: string[] = this.unmarshal();
+                var varnames: string[] = this.unmarshal();
+                var freevars: string[] = this.unmarshal();
+                var cellvars: string[] = this.unmarshal();
+                var filename: string = this.unmarshal();
+                var name: string = this.unmarshal();
+                var firstlineno = this.readInt32();
+                var lnotab: string = this.unmarshal();
+                res = new Code(argc, nlocals, stacksize, flags, codestr, consts,
+                               names, varnames, freevars, cellvars, filename,
+                               name, firstlineno, lnotab);
                 break;
         }
         return res;
