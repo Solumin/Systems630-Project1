0636697aeeaaa5559afc6e2acf483e552cafafe6
Cibele Freire <cibelemf@cs.umass.edu>
2014-10-14 19:05:46 -0400

    do not need these files

--- BrowserFS/test/harness/BackendFactory.ts
+++
@@ -1,7 +0,0 @@
-import file_system = require('../../src/core/file_system');
-
-interface BackendFactory {
-  (cb: (name: string, objs: file_system.FileSystem[]) => void): void;
-}
-
-export = BackendFactory;
