0dbe7faed6b15cf97da1370330a860ad917900e1
Teddy Sudol <magesolumin@gmail.com>
2014-10-19 12:33:19 -0400

    Updated tests

--- examples/int64math.py
+++ examples/int64math.py
@@ -3,4 +3,6 @@ def test(x,y):
 
 a = 2**40
 b = 2**45
+print(a)
+print(b)
 print(test(a,b))
--- examples/play.py
+++ examples/play.py
@@ -1,12 +1,12 @@
 
 # test POP_TOP
 lambda:1
-print (lambda:1)()
+print "Pop lambad:1 = ", (lambda:1)()
 
 # test ROT_TWO
 a = 1; b = 2
 (a, b) = (b, a)
-print a
+print "After (a,b) = (2,1), a = ", a
 
 #test ROT_THREE
 #TODO: make it a bit more meaningfull
@@ -14,23 +14,26 @@ a = 1
 (a, a, a) = (a, a, a)
 
 #test UNARY positive and negative
-b = -3
 a = 2
+b = -3
 
 a = +a
 c = -a
 b = +b
 d = -b
 
-print a, c, b, d
+print "+(2) = ", a
+print "-(2) = ", c
+print "+(-3) = ", b
+print "-(-3) = ", d
 
 #test UNARY not and invert
 a = 0
-print "a =",a
-print not a
+print "a = ", a
+print "not a = ", not a
 
 b = 12
-print ~b
+print "~12 = ", ~b
 
 #test BINARY divide, power and modulo
 a = 5.5 / 2
@@ -39,41 +42,41 @@ b = 3 ** 4
 
 c = 8%3
 
-print a, b, c
+print "5.5 / 2 = ", a
+print "3 ** 4 = ", b
+print "8 % 3 = ", c
 
 #test BINARY subtract, subscr, floor_divide, BUILD_LIST
 
 a = 6
 a = a - 2
-print a
+print "6 - 2 = ", a
 
-c = 7.2
-print c
-
-c = c // 2
-print c
+c = 7.2 // 2
+print "7.2 // 2 = ", c
 
 b = [1,2,3,4]
-print b
+print "A simple list: ", b
 
 #test STORE_subscr and DELETE_subscr
 
+print
 a = [1,2,3]
-print a
+print "Starting list: ", a
 a[1] = 5
-print a
+print "a[1] = 5 => ", a
 del a[1]
-print a
+print "del a[1] => ", a
 
 #test BINARY and, or, xor, lshift, rshift
 
 a = 5
 b = 1
-print a & b
-print a | b
-print a ^ b
-print a << b	
-print a >> b
+print "5 & 1 = ", a & b
+print "5 | 1 = ", a | b
+print "5 ^ 1 = ", a ^ b
+print "5 << 1 = ", a << b	
+print "5 >> 1 = ", a >> b
 
 #test BUILD_map
 
